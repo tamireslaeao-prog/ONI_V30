bfb87649e3ba0bccae815ea15be9023b54746010
tamireslaeao-prog <tamireslaeao@gmail.com>
2026-01-28 13:47:49 -0300

    Delete Modules/AfterEffects/Archive/Legacy_Temp_2026-01-14 directory

--- Modules/AfterEffects/Archive/Legacy_Temp_2026-01-14/connectivity.jsx
+++
@@ -1,5 +0,0 @@
-var f = new File("c:/Users/user/Desktop/connectivity_check.txt");
-f.open("w");
-f.write("AE is alive");
-f.close();
-app.quit();
--- Modules/AfterEffects/Archive/Legacy_Temp_2026-01-14/minimal.jsx
+++
@@ -1,6 +0,0 @@
-var f = new File("c:/Users/user/Desktop/minimal_log.txt");
-f.open("w");
-f.write("Script Started");
-alert("AE Scripting is ALIVE!");
-f.write("\nAlert Shown");
-f.close();
